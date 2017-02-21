d2fb041652453ea631d0508ee52376ccaaa56139
Charles-Xiao <xiaoy24charles@gmail.com>
2017-02-21 21:34:47 +0800

    Setting Webpack, React, Babel and ES6

---
+++ app/main.js
@@ -0,0 +1,4 @@
+eact = require('react');
+let ReactDOM = require('react-dom');
+let AppComponent = require('./components/tab');
+ReactDOM.render(<AppComponent />, document.getElementById('content'));
