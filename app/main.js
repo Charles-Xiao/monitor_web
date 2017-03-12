9b57a6f03d3cc737f188f41aea3c904f1e9b7e8f
Charles-Xiao <xiaoy24charles@gmail.com>
2017-03-12 20:20:28 +0800

    引入Echart

--- app/main.js
+++ app/main.js
@@ -1,7 +1,7 @@
 let React = require('react');
 let ReactDOM = require('react-dom');
-let AppComponent = require('./components/tab');
-ReactDOM.render(<AppComponent />, document.getElementById('content'));
+// let AppComponent = require('./components/tab');
+// ReactDOM.render(<AppComponent />, document.getElementById('content'));
 
 let EchartComponent = require('./components/echart');
 ReactDOM.render(<EchartComponent />, document.getElementById('echart'));
